33c458dc1ef2882fda49e7d7235ef7506391681f
Datrix Tech Solutions <datrixtechsolutions@Chris-MacBook-Pro.local>
2025-02-24 16:23:48 +0000

    login page and stuff

--- src/components/MobileNav.vue
+++ src/components/MobileNav.vue
@@ -2,8 +2,14 @@
 import { PaperClipIcon } from '@heroicons/vue/24/outline';
 import { useRouter } from 'vue-router';
 import { links } from '../data/links';
+import Button from './ui/Button.vue';
+import { useAuthStore } from '../store/auth';
+import { UserIcon } from '@heroicons/vue/24/outline';
+import { ArrowLeftOnRectangleIcon } from '@heroicons/vue/24/outline';
 
 const router = useRouter()
+
+const authStore = useAuthStore()
 
 </script>
 
@@ -44,10 +50,32 @@ const router = useRouter()
             </div>
 
             <!-- buttons -->
-            <!-- <div class="flex py-3">
-                <a href="#" class="button-primary mr-1 w-1/2 text-center">Login</a>
-                <a href="#" class="button-transparent w-1/2 text-center">Sign up</a>
-            </div> -->
+            <div class=" items-center gap-1 group  relative" v-if="authStore.user">
+                <router-link :to="{ name: 'UserAccount' }">
+                    <div class="flex items-center gap-1 mb-5 group  relative">
+                        <div class="rounded-full w-14 h-14 overflow-hidden">
+                            <img src="/images/default-avatar.png" class="w-full h-full" />
+                        </div>
+
+                        <div class="space-y- ">
+                            <p class="font-semibold max-w-[120px] line-clamp-1 text-sm"
+                                :title="authStore.user?.phoneNumber">{{
+                                    authStore.user?.phoneNumber }}</p>
+                            <p class="gray-text text-xs">User logged in</p>
+                        </div>
+                    </div>
+                </router-link>
+                <p @click="authStore.logout()"
+                    class="hover:cursor-pointer bg-red-200 p-2 rounded my-4 text-sm flex items-center justify-center text-red-600 gap-1 hover:text-primary transition relative">
+                    <ArrowLeftOnRectangleIcon class="w-5" /> <span>Logout</span>
+                </p>
+            </div>
+            <div class="flex py-3 justify-center" v-else>
+                <router-link :to="{ name: 'Login' }">
+                    <Button>Login</Button>
+                </router-link>
+                <!-- <a href="#" class="button-transparent w-1/2 text-center">Sign up</a> -->
+            </div>
         </div>
     </nav>
 </template>
